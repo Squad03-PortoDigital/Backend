092bbe6f221fc032844c5ab00e3fd06e35db84e2
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2025-08-25 08:58:01 -0300

    initial commit

---
+++ src/test/java/com/squad03/flap/FlapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad03.flap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
