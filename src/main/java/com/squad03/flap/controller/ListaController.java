4aaa8f5ac886ee8c2279e0c3a0ea2cacc8c598b7
Luan Cortes <luancortes208@gmail.com>
2025-09-11 10:11:55 -0300

    feat: controller lista

---
+++ src/main/java/com/squad03/flap/controller/ListaController.java
@@ -0,0 +1,52 @@
+package com.squad03.flap.controller;
+
+import com.squad03.flap.DTO.AtualizacaoLista;
+import com.squad03.flap.DTO.BuscaLista;
+import com.squad03.flap.DTO.CadastroLista;
+import com.squad03.flap.model.Lista;
+import com.squad03.flap.service.ListaService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/listas")
+public class ListaController {
+    
+    private ListaService listaService;
+    
+    @Autowired
+    public ListaController(ListaService listaService) {
+        this.listaService = listaService;
+    }
+
+    @PostMapping
+    @Transactional
+    public void createLista(@RequestBody CadastroLista dados) {
+        listaService.cadastrarLista(dados);
+    }
+
+    @GetMapping
+    public List<BuscaLista> findAll() {
+        return listaService.buscarListas();
+    }
+
+    @GetMapping("/{id}")
+    public Lista findById(@RequestParam int id) {
+        return listaService.buscarListaPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public void updateLista(@PathVariable int id, @RequestBody AtualizacaoLista dados) {
+        listaService.AtualizarLista(id, dados);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public void deleteLista(@PathVariable int id) {
+        listaService.excluirLista(id);
+    }
+}
