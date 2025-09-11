5809dd7dc6cb8ff3a05d0fb74d56527c778e7497
Luan Cortes <luancortes208@gmail.com>
2025-09-11 10:09:04 -0300

    excluindo dtos sem utilidades

--- src/main/java/com/squad03/flap/DTO/Cargo.java
+++
@@ -1,4 +0,0 @@
-package com.squad03.flap.DTO;
-
-public record Cargo(int id, String nome) {
-}
--- src/main/java/com/squad03/flap/DTO/Quadro.java
+++
@@ -1,4 +0,0 @@
-package com.squad03.flap.DTO;
-
-public record Quadro(int id, String nome, String foto, String status) {
-}
