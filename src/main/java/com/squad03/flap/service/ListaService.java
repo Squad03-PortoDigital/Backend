523f421ec8c3bd75fb0db162fdba837a9abaca7f
Luan Cortes <luancortes208@gmail.com>
2025-09-13 13:28:49 -0300

    refactor: Aprimorando código

--- src/main/java/com/squad03/flap/service/ListaService.java
+++ src/main/java/com/squad03/flap/service/ListaService.java
@@ -1,14 +1,17 @@
 package com.squad03.flap.service;
 
 import com.squad03.flap.DTO.AtualizacaoLista;
+import com.squad03.flap.DTO.BuscaEmpresa;
 import com.squad03.flap.DTO.CadastroLista;
 import com.squad03.flap.DTO.BuscaLista;
 import com.squad03.flap.model.Lista;
 import com.squad03.flap.repository.ListaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,28 +24,34 @@ public class ListaService {
         this.repository = repository;
     }
 
-    public void cadastrarLista(CadastroLista cadastroLista) {
-        repository.save(new Lista(cadastroLista));
+    @Transactional
+    public BuscaLista cadastrarLista(CadastroLista cadastroLista) {
+        Lista listaNova = repository.save(new Lista(cadastroLista));
+        return new BuscaLista(listaNova);
     }
 
-    public Lista buscarListaPorId(int id) {
-        return repository.findById(id).get();
+    public Optional<BuscaLista> buscarListaPorId(int id) {
+        return repository.findById(id).map(BuscaLista::new);
     }
 
     public List<BuscaLista> buscarListas() {
         return repository.findAll().stream().map(BuscaLista::new).collect(Collectors.toList());
     }
 
-    public void AtualizarLista(int id, AtualizacaoLista atualizacaoLista) {
+    @Transactional
+    public BuscaLista AtualizarLista(int id, AtualizacaoLista atualizacaoLista) {
         Lista Lista = repository.findById(id).get();
 
         Lista.setNome(atualizacaoLista.nome());
         Lista.setPosicao(atualizacaoLista.posicao());
         Lista.setCor(atualizacaoLista.cor());
 
-        repository.save(Lista);
+        Lista novaLista = repository.save(Lista);
+
+        return new BuscaLista(novaLista);
     }
 
+    @Transactional
     public void excluirLista(int id) {
         repository.deleteById(id);
     }
