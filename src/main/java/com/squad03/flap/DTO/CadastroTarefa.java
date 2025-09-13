9d94d459d2c281146116f500cadc711cfb8c353c
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-09-13 19:00:29 -0300

    Refactor DTOs
    renomeação dos DTOs e usages para padronização

--- src/main/java/com/squad03/flap/DTO/CadastroTarefa.java
+++ src/main/java/com/squad03/flap/DTO/CadastroTarefa.java
@@ -1,14 +1,13 @@
 package com.squad03.flap.DTO;
 
-import com.squad03.flap.model.Tarefa.StatusTarefa;
 import com.squad03.flap.model.Tarefa.PrioridadeTarefa;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record AtualizarTarefaDTO(
+public record CadastroTarefa(
+    Long agenteId,
     String titulo,
     String descricao,
-    StatusTarefa status,
     PrioridadeTarefa prioridade,
     LocalDateTime dtEntrega,
     List<String> tags,
