9ea6fcef8c7e6f14347d56c6687864c941df8608
Luan Cortes <luancortes208@gmail.com>
2025-09-13 13:25:08 -0300

    refactor: Adicionando Id no DTO

--- src/main/java/com/squad03/flap/DTO/BuscaEmpresa.java
+++ src/main/java/com/squad03/flap/DTO/BuscaEmpresa.java
@@ -2,8 +2,8 @@ package com.squad03.flap.DTO;
 
 import com.squad03.flap.model.Empresa;
 
-public record BuscaEmpresa(String nome, String foto) {
+public record BuscaEmpresa(int id, String nome, String foto) {
     public BuscaEmpresa(Empresa empresa) {
-        this(empresa.getNome(), empresa.getFoto());
+        this(empresa.getId(), empresa.getNome(), empresa.getFoto());
     }
 }
--- src/main/java/com/squad03/flap/DTO/BuscaLista.java
+++ src/main/java/com/squad03/flap/DTO/BuscaLista.java
@@ -2,8 +2,8 @@ package com.squad03.flap.DTO;
 
 import com.squad03.flap.model.Lista;
 
-public record BuscaLista(String nome, int posicao, String cor) {
+public record BuscaLista(int id, String nome, int posicao, String cor) {
     public BuscaLista (Lista lista){
-        this(lista.getNome(), lista.getPosicao(), lista.getCor());
+        this(lista.getId(), lista.getNome(), lista.getPosicao(), lista.getCor());
     }
 }
