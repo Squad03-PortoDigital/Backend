1617bd6c203921b6ef451463c78600fbd8b7882d
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-09-13 19:04:04 -0300

    Remoção de arquivos para evitar conflitos

--- src/main/java/com/squad03/flap/DTO/Tarefa.java
+++
@@ -1,5 +0,0 @@
-package com.squad03.flap.DTO;
-
-public class Tarefa {
-    
-}
--- src/main/java/com/squad03/flap/DTO/Usuario.java
+++
@@ -1,4 +0,0 @@
-package com.squad03.flap.DTO;
-
-public record Usuario(int id, String foto, String nome, String email) {
-}
