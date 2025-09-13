6979805739baf7761628a96080789e4d67fc5047
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-09-12 23:09:47 -0300

    Create TarefaRepository.java

---
+++ src/main/java/com/squad03/flap/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package com.squad03.flap.repository;
+
+import com.squad03.flap.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
