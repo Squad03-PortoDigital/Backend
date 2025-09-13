de6ab3873997b91934a93355e3273c5d6e0c7701
Luan Cortes <luancortes208@gmail.com>
2025-09-13 13:23:54 -0300

    refactor: atualizando o controller

--- src/main/java/com/squad03/flap/controller/ListaController.java
+++ src/main/java/com/squad03/flap/controller/ListaController.java
@@ -3,13 +3,15 @@ package com.squad03.flap.controller;
 import com.squad03.flap.DTO.AtualizacaoLista;
 import com.squad03.flap.DTO.BuscaLista;
 import com.squad03.flap.DTO.CadastroLista;
-import com.squad03.flap.model.Lista;
 import com.squad03.flap.service.ListaService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/listas")
@@ -23,30 +25,32 @@ public class ListaController {
     }
 
     @PostMapping
-    @Transactional
-    public void createLista(@RequestBody CadastroLista dados) {
-        listaService.cadastrarLista(dados);
+    public ResponseEntity<BuscaLista> createLista(@RequestBody CadastroLista dados) {
+        BuscaLista lista = listaService.cadastrarLista(dados);
+        return new ResponseEntity<>(lista,HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<BuscaLista> findAll() {
-        return listaService.buscarListas();
+    public ResponseEntity<List<BuscaLista>> findAll() {
+        List<BuscaLista> listas = listaService.buscarListas();
+        return ResponseEntity.ok().body(listas);
     }
 
     @GetMapping("/{id}")
-    public Lista findById(@RequestParam int id) {
-        return listaService.buscarListaPorId(id);
+    public ResponseEntity<BuscaLista> findById(@PathVariable int id) {
+        Optional<BuscaLista> lista = listaService.buscarListaPorId(id);
+        return lista.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    @Transactional
-    public void updateLista(@PathVariable int id, @RequestBody AtualizacaoLista dados) {
-        listaService.AtualizarLista(id, dados);
+    public ResponseEntity<BuscaLista> updateLista(@PathVariable int id, @RequestBody AtualizacaoLista dados) {
+        BuscaLista lista = listaService.AtualizarLista(id, dados);
+        return ResponseEntity.ok().body(lista);
     }
 
     @DeleteMapping
-    @Transactional
-    public void deleteLista(@PathVariable int id) {
+    public ResponseEntity<Void> deleteLista(@PathVariable int id) {
         listaService.excluirLista(id);
+        return ResponseEntity.noContent().build();
     }
 }
