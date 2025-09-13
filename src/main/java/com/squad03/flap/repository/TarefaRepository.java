a33417c254c306d6ed1822349687ea3141d207a2
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-09-13 19:18:46 -0300

    Arquivos deletados para evitar conflito

--- src/main/java/com/squad03/flap/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.squad03.flap.repository;
-
-import com.squad03.flap.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
