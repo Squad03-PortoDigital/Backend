2e744e8647951725155e1b2a100dd71d2d70a218
Luan Cortes <luancortes208@gmail.com>
2025-09-15 16:07:34 -0300

    refactor: add novos atributos

--- src/main/java/com/squad03/flap/DTO/BuscaEmpresa.java
+++ src/main/java/com/squad03/flap/DTO/BuscaEmpresa.java
@@ -2,8 +2,11 @@ package com.squad03.flap.DTO;
 
 import com.squad03.flap.model.Empresa;
 
-public record BuscaEmpresa(int id, String nome, String foto) {
+public record BuscaEmpresa(int id, String nome, String cnpj, String email, String contato,
+                           String atuacao, String observacao, String foto) {
     public BuscaEmpresa(Empresa empresa) {
-        this(empresa.getId(), empresa.getNome(), empresa.getFoto());
+
+        this(empresa.getId(), empresa.getNome(), empresa.getCnpj(), empresa.getEmail(),
+                empresa.getContato(), empresa.getAtuacao(), empresa.getObservacao(), empresa.getFoto());
     }
 }
