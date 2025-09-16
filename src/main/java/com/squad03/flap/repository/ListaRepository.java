dd5127482866095a637c69997aaabcec070ebfaf
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-09-16 19:28:10 -0300

    Create ListaRepository.java

---
+++ src/main/java/com/squad03/flap/repository/ListaRepository.java
@@ -0,0 +1,7 @@
+package com.squad03.flap.repository;
+
+import com.squad03.flap.model.Lista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListaRepository extends JpaRepository<Lista, Integer> {
+}
