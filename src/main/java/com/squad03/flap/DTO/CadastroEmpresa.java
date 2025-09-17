25c1971095492be2e7e08d4a925ed9467de51b4b
Luan Cortes <luancortes208@gmail.com>
2025-09-17 11:37:37 -0300

    feat: adicionando annotations de validacao

--- src/main/java/com/squad03/flap/DTO/CadastroEmpresa.java
+++ src/main/java/com/squad03/flap/DTO/CadastroEmpresa.java
@@ -1,5 +1,31 @@
 package com.squad03.flap.DTO;
 
-public record CadastroEmpresa(String nome, String cnpj, String email, String contato,
-                              String atuacao, String observacao, String foto) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CadastroEmpresa(@NotBlank(message = "O nome não pode estar em branco.")
+                              @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
+                              String nome,
+
+                              @NotBlank(message = "O CNPJ не pode estar em branco.")
+                              @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter apenas 14 números.")
+                              String cnpj,
+
+                              @NotBlank(message = "O e-mail não pode estar em branco.")
+                              @Email(message = "O formato do e-mail é inválido.")
+                              String email,
+
+                              @NotBlank(message = "O contato не pode estar em branco.")
+                              @Size(min = 10, max = 15, message = "O contato deve ter entre 10 e 15 caracteres (com DDD).")
+                              String contato,
+
+                              @NotBlank(message = "A área de atuação не pode estar em branco.")
+                              String atuacao,
+
+                              String observacao,
+
+                              String foto)
+                              {
 }
--- src/main/java/com/squad03/flap/DTO/CadastroLista.java
+++ src/main/java/com/squad03/flap/DTO/CadastroLista.java
@@ -1,4 +1,18 @@
 package com.squad03.flap.DTO;
 
-public record CadastroLista(String nome, int posicao, String cor) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record CadastroLista(
+        @NotBlank(message = "O nome não pode estar em branco.")
+        @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
+        String nome,
+
+        @NotNull
+        @Positive
+        int posicao,
+
+        String cor) {
 }
