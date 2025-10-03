3b36dddcf97f1ab7d8a1b9269c63fe3f215748df
Luan Cortes <luancortes208@gmail.com>
2025-10-03 13:40:13 -0300

    feat: Criando DTO movimentacao

---
+++ src/main/java/com/squad03/flap/DTO/AtualizacaoMovimentacao.java
@@ -0,0 +1,4 @@
+package com.squad03.flap.DTO;
+
+public record AtualizacaoMovimentacao(Long role_id, Long listaOrigem_id, Long listaDestino_id) {
+}
---
+++ src/main/java/com/squad03/flap/DTO/BuscaMovimentacao.java
@@ -0,0 +1,12 @@
+package com.squad03.flap.DTO;
+
+import com.squad03.flap.model.Lista;
+import com.squad03.flap.model.Movimentacao;
+import com.squad03.flap.model.Role;
+
+public record BuscaMovimentacao(Long id, Role role_id, Lista listaOrigem_id, Lista listaDestino_id) {
+    public BuscaMovimentacao (Movimentacao movimentacao){
+        this(movimentacao.getId(), movimentacao.getRole(), movimentacao.getListaOrigem(),
+                movimentacao.getListaDestino());
+    }
+}
