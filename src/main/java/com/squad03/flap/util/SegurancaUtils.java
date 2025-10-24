165006ddb184a913986b1972dfdb38c344524783
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-10-24 16:28:25 -0300

    Create SegurancaUtils.java

---
+++ src/main/java/com/squad03/flap/util/SegurancaUtils.java
@@ -0,0 +1,18 @@
+package com.squad03.flap.util;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SegurancaUtils {
+    public String getUsuarioLogadoEmail() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
+    }
+}
