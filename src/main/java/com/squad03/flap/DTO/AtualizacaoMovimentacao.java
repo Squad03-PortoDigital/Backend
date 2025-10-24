0c9a97d3df1229ab7ddad379667639d11bbef7fa
Gabriel Marques Fiel <Gabriel.m.fiel@gmail.com>
2025-10-24 16:28:07 -0300

    Delete entidade movimentação

--- src/main/java/com/squad03/flap/DTO/AtualizacaoMovimentacao.java
+++
@@ -1,4 +0,0 @@
-package com.squad03.flap.DTO;
-
-public record AtualizacaoMovimentacao(Long role_id, Long listaOrigem_id, Long listaDestino_id) {
-}
--- src/main/java/com/squad03/flap/repository/MovimentacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.squad03.flap.repository;
-
-import com.squad03.flap.model.Movimentacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
-}
